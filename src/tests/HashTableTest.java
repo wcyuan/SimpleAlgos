77c8c3901df670cfcae3d8b5662ffac8e6458c15
Conan Yuan <conanyuan@gmail.com>
2014-05-20 21:06:18 -0400

    format again

--- src/tests/HashTableTest.java
+++ src/tests/HashTableTest.java
@@ -4,6 +4,7 @@
 
 package tests;
 
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -11,35 +12,37 @@ import org.junit.Test;
 import algos.HashTable;
 
 /**
- *
+ * Tests for HashTables
  */
-public class HashTableTest {
+public class HashTableTest
+{
 
-        /**
-         * Basic tests
-         */
-        @Test
-        public void test() {
-                HashTable<String, Integer> ht = new HashTable<String, Integer>(5);
-                assertEquals("{}", ht.toString());
-                assertEquals(null, ht.get("Goodbye"));
-                ht.set("Hello", 4);
-                assertEquals(4, (int)ht.get("Hello"));
-                assertEquals(null, ht.get("Goodbye"));
-                assertEquals("{(Hello, 4)}", ht.toString());
-                ht.set("Goodbye", 5);
-                assertEquals(4, (int)ht.get("Hello"));
-                assertEquals(5, (int)ht.get("Goodbye"));
-                assertEquals(null, ht.get("asdf"));
-                assertEquals("{(Hello, 4), (Goodbye, 5)}", ht.toString());
-                ht.set("Hello", 2);
-                assertEquals(2, (int)ht.get("Hello"));
-                assertEquals(5, (int)ht.get("Goodbye"));
-                assertEquals(null, ht.get("asdf"));
-                assertEquals("{(Hello, 2), (Goodbye, 5)}", ht.toString());
-                ht.delete("Hello");
-                assertEquals(null, ht.get("Hello"));
-                assertEquals(5, (int)ht.get("Goodbye"));
-                assertEquals("{(Goodbye, 5)}", ht.toString());
-        }
+    /**
+     * Basic tests
+     */
+    @Test
+    public void test()
+    {
+        HashTable<String, Integer> ht = new HashTable<String, Integer>(5);
+        assertEquals("{}", ht.toString());
+        assertEquals(null, ht.get("Goodbye"));
+        ht.set("Hello", 4);
+        assertEquals(4, (int)ht.get("Hello"));
+        assertEquals(null, ht.get("Goodbye"));
+        assertEquals("{(Hello, 4)}", ht.toString());
+        ht.set("Goodbye", 5);
+        assertEquals(4, (int)ht.get("Hello"));
+        assertEquals(5, (int)ht.get("Goodbye"));
+        assertEquals(null, ht.get("asdf"));
+        assertEquals("{(Hello, 4), (Goodbye, 5)}", ht.toString());
+        ht.set("Hello", 2);
+        assertEquals(2, (int)ht.get("Hello"));
+        assertEquals(5, (int)ht.get("Goodbye"));
+        assertEquals(null, ht.get("asdf"));
+        assertEquals("{(Hello, 2), (Goodbye, 5)}", ht.toString());
+        ht.delete("Hello");
+        assertEquals(null, ht.get("Hello"));
+        assertEquals(5, (int)ht.get("Goodbye"));
+        assertEquals("{(Goodbye, 5)}", ht.toString());
+    }
 }
--- src/tests/LinkedListTest.java
+++ src/tests/LinkedListTest.java
@@ -4,6 +4,7 @@
 
 package tests;
 
+
 import static org.junit.Assert.*;
 
 import java.util.Iterator;
@@ -15,72 +16,75 @@ import algos.LinkedList;
 /**
  * Tests for Linked Lists
  */
-public class LinkedListTest {
+public class LinkedListTest
+{
 
-        /**
-         * Test toString, insert, and pop
-         */
-        @Test
-        public void testBasic() {
-                LinkedList<Integer> list = new LinkedList<Integer>();
-                assertEquals("[]", list.toString());
-                assertEquals(0, list.size());
-                list.insert(5);
-                assertEquals("[5]", list.toString());
-                assertEquals(1, list.size());
-                list.insert(6);
-                assertEquals("[6, 5]", list.toString());
-                assertEquals(2, list.size());
-                list.pop();
-                assertEquals("[5]", list.toString());
-                assertEquals(1, list.size());
-                list.pop();
-                assertEquals("[]", list.toString());
-                assertEquals(0, list.size());
-        }
+    /**
+     * Test toString, insert, and pop
+     */
+    @Test
+    public void testBasic()
+    {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        assertEquals("[]", list.toString());
+        assertEquals(0, list.size());
+        list.insert(5);
+        assertEquals("[5]", list.toString());
+        assertEquals(1, list.size());
+        list.insert(6);
+        assertEquals("[6, 5]", list.toString());
+        assertEquals(2, list.size());
+        list.pop();
+        assertEquals("[5]", list.toString());
+        assertEquals(1, list.size());
+        list.pop();
+        assertEquals("[]", list.toString());
+        assertEquals(0, list.size());
+    }
 
-        /**
-         * Test removing an element
-         */
-        @Test
-        public void testRemove() {
-                LinkedList<Integer> list = new LinkedList<Integer>();
-                list.insert(3);
-                list.insert(2);
-                list.insert(1);
+    /**
+     * Test removing an element
+     */
+    @Test
+    public void testRemove()
+    {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.insert(3);
+        list.insert(2);
+        list.insert(1);
 
-                Iterator<Integer> itr = list.iterator();
-                assertEquals(3, list.size());
-                assertEquals("[1, 2, 3]", list.toString());
-                assertEquals(true, itr.hasNext());
-                itr.remove();
-                assertEquals(3, list.size());
-                assertEquals("[1, 2, 3]", list.toString());
-                assertEquals(true, itr.hasNext());
+        Iterator<Integer> itr = list.iterator();
+        assertEquals(3, list.size());
+        assertEquals("[1, 2, 3]", list.toString());
+        assertEquals(true, itr.hasNext());
+        itr.remove();
+        assertEquals(3, list.size());
+        assertEquals("[1, 2, 3]", list.toString());
+        assertEquals(true, itr.hasNext());
 
-                assertEquals(1, (int)itr.next());
-                itr.remove();
-                assertEquals(2, list.size());
-                assertEquals("[2, 3]", list.toString());
-                assertEquals(true, itr.hasNext());
-                itr.remove();
-                assertEquals(2, list.size());
-                assertEquals("[2, 3]", list.toString());
-                assertEquals(true, itr.hasNext());
+        assertEquals(1, (int)itr.next());
+        itr.remove();
+        assertEquals(2, list.size());
+        assertEquals("[2, 3]", list.toString());
+        assertEquals(true, itr.hasNext());
+        itr.remove();
+        assertEquals(2, list.size());
+        assertEquals("[2, 3]", list.toString());
+        assertEquals(true, itr.hasNext());
 
-                assertEquals(2, (int)itr.next());
-                itr.remove();
-                assertEquals(1, list.size());
-                assertEquals("[3]", list.toString());
-                assertEquals(true, itr.hasNext());
+        assertEquals(2, (int)itr.next());
+        itr.remove();
+        assertEquals(1, list.size());
+        assertEquals("[3]", list.toString());
+        assertEquals(true, itr.hasNext());
 
-                assertEquals(3, (int)itr.next());
-                itr.remove();
-                assertEquals(0, list.size());
-                assertEquals("[]", list.toString());
-                itr.remove();
-                assertEquals(0, list.size());
-                assertEquals("[]", list.toString());
-                assertEquals(false, itr.hasNext());
-        }
+        assertEquals(3, (int)itr.next());
+        itr.remove();
+        assertEquals(0, list.size());
+        assertEquals("[]", list.toString());
+        itr.remove();
+        assertEquals(0, list.size());
+        assertEquals("[]", list.toString());
+        assertEquals(false, itr.hasNext());
+    }
 }
